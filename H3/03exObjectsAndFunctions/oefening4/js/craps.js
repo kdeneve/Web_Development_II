e730885b77739b04d0cfa25c0151423aff492c43
Kurt De Neve <kdeneve@outlook.be>
2024-07-16 17:56:03 +0200

    gestart met H4 Afrikaans dobeelen

--- H3/03exObjectsAndFunctions/oefening4/js/craps.js
+++ H3/03exObjectsAndFunctions/oefening4/js/craps.js
@@ -1,44 +1,67 @@
+"use strict";
+
 const dice1 = {
   eyes: 1,
-  roll: function () {
+  roll() {
     this.eyes = Math.floor(Math.random() * 6 + 1);
   },
 };
 
-const dice2 = {
-  eyes: 1,
-  roll: function () {
-    this.eyes = Math.floor(Math.random() * 6 + 1);
-  },
-};
-
-export const initialiseerCraps =function() {
-  document.getElementById("play").onclick = play;
-  craps.bet = prompt("What is your bet?");
-  alert("Press play to start the game");
-}
+const dice2 = {};
+({ eyes: dice2.eyes, roll: dice2.roll }) = dice1;
 
 const craps = {
-  bet: 0,
-  point: 0,
-  earned: 0,
+  bet: 0, //inzet
+  point: 0, //punten eerste worp
   gameover: false,
+  earned: 0, //gewonnen
   dices: [dice1, dice2],
   rollDice: function () {
-    dice1.roll();
-    dice2.roll();
+    for (const dice of this.dices) dice.roll();
   },
   getSum: function () {
-    return dice1.eyes + dice2.eyes;
-  }
-  
+    let sum = 0;
+    for (const dice of this.dices) {
+      sum += dice.eyes;
+    }
+    return sum;
+  },
+  play: function () {
+    this.rollDice();
+    const sum = this.getSum();
+    if (this.point === 0) {
+      if (sum === 7 || sum === 11) {
+        this.gameover = true;
+        this.earned = this.bet * 2;
+      } else this.point = sum;
+    } else {
+      if (sum === this.point) {
+        this.gameover = true;
+        this.earned = this.bet;
+      } else {
+        if (sum === 7 || sum === 11) {
+          this.gameover = true;
+          this.earned = 0;
+        }
+      }
+    }
+  },
+};
 
-  }
-    // if (craps.getSum() == 7 || craps.getSum() == 11 )
-   // this.bet *= 2;
-    // else
+function play() {
+  craps.play();
 
-const play = function () {
-  craps.rollDice();
-  alert(`You played ${craps.getSum()}`);
+  let message = `You played ${craps.getSum()}.`;
+
+  if (craps.gameover) {
+    document.getElementById("play").disabled = true;
+    if (craps.earned === 0) message += " You lost your bet.";
+    else message += ` You won ${craps.earned}.`;
+  } else message += ` Keep on playing. Your point is ${craps.point}.`;
+  window.alert(message);
+}
+export function initialiseerCraps() {
+  document.getElementById("play").onclick = play;
+  craps.bet = parseInt(prompt("What is your bet?"));
+  alert("Press play to start the game");
 }
---
+++ H4/04exOOP_In_JavaScript/AfrikaansDobbelen/js/Dobbelsteen.js
@@ -0,0 +1,7 @@
+class Dobbelsteen {
+  
+  #aantalOgen = 1;
+  rol() { this.#aantalOgen = Math.floor(Math.random() * 6 + 1);}
+
+
+}
