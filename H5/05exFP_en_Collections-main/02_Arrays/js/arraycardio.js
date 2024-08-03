00e30f130fc02c54901a605fdd80c4ec00f8347b
Kurt De Neve <kdeneve@outlook.be>
2024-08-03 23:43:15 +0200

    H5 oef array02 tot nr 6

--- H5/05exFP_en_Collections-main/02_Arrays/js/arraycardio.js
+++ H5/05exFP_en_Collections-main/02_Arrays/js/arraycardio.js
@@ -69,7 +69,9 @@ console.table(fifteen);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventor first and last names
-const fullNames = inventors.map((inventor) => inventor.first + " " + inventor.last)
+const fullNames = inventors.map(
+  (inventor) => inventor.first + " " + inventor.last
+);
 console.log(fullNames);
 
 // Array.prototype.sort()
@@ -82,23 +84,30 @@ console.log(fullNames);
 //   }
 // });
 
-const ordered = inventors.sort((a, b) => a.year - b.year)
-
-
+const ordered = inventors.sort((a, b) => a.year - b.year);
 console.table(ordered);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
-const totalYears = inventors.reduce((pv, inventor) => pv + inventor.passed - inventor.year, 0);
+const totalYears = inventors.reduce(
+  (pv, inventor) => pv + inventor.passed - inventor.year,
+  0
+);
 console.log(totalYears);
 
 // 5. Sort the inventors by years lived
-const oldest = inventors.sort((a, b) => (b.passed - b.year) - (a.passed - a.year));
+const oldest = inventors.sort(
+  (a, b) => b.passed - b.year - (a.passed - a.year)
+);
 console.table(oldest);
 
 // 6. Sort Exercise
 // Sort the people alphabetically by last name
-const alpha = undefined;
+const alpha = people.sort((lastOne, nextOne) => {
+  const [aLast, aFirst] = lastOne.split(", ");
+  const [bLast, bFirst] = nextOne.split(", ");
+  return aLast > bLast ? 1 : -1;
+});
 console.log(alpha);
 
 // 7. Reduce Exercise
