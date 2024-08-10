fe7923cd54dfae25f30572e03c5e0c58fb11b5fd
Kurt De Neve <kdeneve@outlook.be>
2024-08-10 21:46:43 +0200

    H5 start easy maps oefening

--- H5/05exFP_en_Collections-main/05_MapsEnSets_Easy/js/mapsEnSetsEasy.js
+++ H5/05exFP_en_Collections-main/05_MapsEnSets_Easy/js/mapsEnSetsEasy.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 // =============
 // Deel 1 - Maps
 // =============
@@ -12,6 +12,13 @@ const nl_en_dict = new Map();
 // kaars - candle
 // dubbel - double
 
+nl_en_dict
+  .set("stoel", "chair")
+  .set("tafel", "table")
+  .set("kaars", "candle")
+  .set("dubbel", "double");
+
+console.log(nl_en_dict);
 
 // Declareer een map genaamd en_fr_dict die initieel volgende entries bevat.
 // chair - chaise
@@ -19,28 +26,39 @@ const nl_en_dict = new Map();
 // candle - bougie
 // double - double
 
+const en_fr_dict = new Map();
+
+en_fr_dict
+  .set("chair", "chaise")
+  .set("table", "table")
+  .set("candle", "kaars")
+  .set("double", "double");
+
+console.log(en_fr_dict);
 
 // Gebruik nl_en_dict om woorden te vertalen van NL naar EN
 // Vul onderstaande code aan
-console.log('Vertalen van NL naar EN');
-let word = prompt('Geef een nederlands woord');
-while (word) {
-  if (/** hier aanvullen **/)
-    console.log(`NL: ${word} - EN: ${/** hier aanvullen **/}`);
-  else console.log(`${word} staat niet in het woordenboek`);
-  word = prompt('Geef een nederlands woord');
-}
+
+// console.log('Vertalen van NL naar EN');
+// let word = prompt('Geef een nederlands woord');
+// while (word) {
+//   if (true)
+//     console.log(`NL: ${word} - EN: ${/** hier aanvullen **/}`);
+//   else console.log(`${word} staat niet in het woordenboek`);
+//   word = prompt('Geef een nederlands woord');
+// }
 
 // Gebruik beide woordenboeken om woorden te vertalen van NL naar EN
 // Vul onderstaande code aan
-console.log('Vertalen van NL naar FR');
-word = prompt('Geef een nederlands woord');
-while (word) {
-  if (/** hier aanvullen **/)
-    console.log(`NL: ${word} - FR: ${/** hier aanvullen **/}`);
-  else console.log(`${word} staat niet in het woordenboek`);
-  word = prompt('Geef een nederlands woord');
-}
+
+// console.log('Vertalen van NL naar FR');
+// word = prompt('Geef een nederlands woord');
+// while (word) {
+//   if (/** hier aanvullen **/)
+//     console.log(`NL: ${word} - FR: ${/** hier aanvullen **/}`);
+//   else console.log(`${word} staat niet in het woordenboek`);
+//   word = prompt('Geef een nederlands woord');
+// }
 
 // Itereer over nl_en_dict en log alle nederlandse woorden naar de console.
 // Maak gebruik van een for .. of lus
@@ -49,10 +67,8 @@ while (word) {
 // Maak gebruik van een for .. of lus en array destructuring.
 // Elke entry wordt getoond als key: value
 
-
 // Schrijf een functie die een woordenboek als parameter heeft
 // en die retourneert hoeveel woorden identiek zijn in beide talen (i.e. key en value hebben dezelfde waarde)
-
 
 // Gebruik de functie en log naar de console voor onze beide dictionaries
 // hoeveel identieke woorden ze bevatten
@@ -62,15 +78,15 @@ while (word) {
 // =============
 
 const krishna = [
-  'Hare',
-  'Krishna',
-  'Hare',
-  'Krishna',
-  'Krishna',
-  'Krishna',
-  'Hare',
-  'Hare',
-  ':-O'
+  "Hare",
+  "Krishna",
+  "Hare",
+  "Krishna",
+  "Krishna",
+  "Krishna",
+  "Hare",
+  "Hare",
+  ":-O",
 ];
 
 // Overloop de array krishna met een forEach en voeg alle
@@ -81,6 +97,4 @@ const krishna = [
 // Maak nu gebruik van de spread syntax om met een one-liner
 // een *array* van unieke waarden te krijgen uit de array krishna
 
-
 // Maak een array met 3 *verschillende* random getallen tussen 1 en 6
-
---
+++ H5/05exFP_en_Collections-main/05_MapsEnSets_Easy/js/mapsEnSetsEasy_test.js
@@ -0,0 +1,9 @@
+"use strict";
+const nl_en_dict = new Map();
+nl_en_dict
+  .set("stoel", "chair")
+  .set("tafel", "table")
+  .set("kaars", "candle")
+  .set("dubbel", "double");
+console.log("kurt");
+console.log(nl_en_dict);
